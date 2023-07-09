14e361e05a580d87dd6f8af1e61ca6fadb86ed5d
Jiacheng-coder <2253541907@qq.com>
2023-07-09 20:00:59 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react"
 import Demo from './components/hooks-useInput'
-import ImmerjsDemo from "./components/immerjs-demo"
-const useMouse = () => {
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  })
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setPosition({
-        x: e.pageX,
-        y: e.pageY
-      })
-    }
-    document.addEventListener('mousemove', handleMouseMove)
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove)
-    }
-  }, [])
-  return position
-}
+import ImmerjsDemo from './components/immerjs-demo'
 
 function App() {
-  return <div>
-    <h1>Demo</h1>
-    <Demo />
-    <ImmerjsDemo />
-  </div>
+  const a = 12
+  return (
+    <div>
+      <h1>Demo</h1>
+      <Demo />
+      <ImmerjsDemo />
+    </div>
+  )
 }
 
 export default App
