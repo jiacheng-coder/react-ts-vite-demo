18bf67f17a47f98cdcc17f6061897187c9387d3c
Jiacheng-coder <2253541907@qq.com>
2023-07-15 17:02:44 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import Demo from './components/hooks-useInput'
 import ImmerjsDemo from './components/immerjs-demo'
+import Home from './pages/Home'
+
+type SuccessProps = {
+  status: 'success'
+  data: string
+}
+type FailProps = {
+  status: 'fail'
+  message: string
+}
+type Props = {
+  time: Date
+} & (SuccessProps | FailProps)
 
 function App() {
   const a = 12
+  const res: Props = {
+    time: new Date(),
+    status: 'success',
+    data: 'It works',
+  }
   return (
-    <div>
+    <>
       <h1>Demo</h1>
       <Demo />
       <ImmerjsDemo />
-    </div>
+      <Home />
+    </>
   )
 }
 
